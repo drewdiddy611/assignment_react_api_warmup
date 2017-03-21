f691def5b63850858fa2889019ec842ff2c1a3d6
Sia <siakaramalegos@gmail.com>
2017-03-21 15:02:39 -0500

    Add create user and refactor into app container

--- src/components/App.js
+++ src/components/App.js
@@ -2,47 +2,11 @@ import React, { Component } from 'react'
 import JumbotronFluid from './elements/JumbotronFluid'
 import UserList from './UserList'
 import UserForm from './UserForm'
-import serialize from 'form-serialize'
 
 class App extends Component {
-  constructor() {
-    super()
-
-    // Initialize users in state as an empty array and
-    // set isFetching to false.
-    this.state = {
-      users: [],
-      isFetching: false,
-    }
-  }
-
-  componentDidMount() {
-    // Before performing the fetch, set isFetching to true
-    this.setState({isFetching: true})
-
-    // After component mounts, call the API to get the
-    // users, then update state which triggers re-render.
-    // Add a delay to the URL and reset isFetching upon
-    // completion of the request.
-    fetch('https://reqres.in/api/users?delay=3')
-      .then((response) => response.json())
-      .then((json) => {
-        this.setState({
-          users: json.data,
-          isFetching: false,
-        })
-      })
-  }
-
-  onAddUser = (e) => {
-    e.preventDefault()
-    const form = e.target
-    const body = serialize(form, {hash: true})
-    console.log(body)
-  }
 
   render() {
-    const {users, isFetching} = this.state
+    const {users, isFetching, error, onAddUser} = this.props
 
     return (
       <div className="App">
@@ -50,9 +14,15 @@ class App extends Component {
           heading="User CRUD"
           lead="Using an API for User CRUD operations in React Applications"
         />
-        <UserList users={users} isFetching={isFetching} />
+        <UserList
+          users={users}
+          isFetching={isFetching}
+        />
         <br />
-        <UserForm onSubmit={this.onAddUser} />
+        <UserForm
+          onSubmit={onAddUser}
+          error={error}
+        />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import AppContainer from './containers/AppContainer';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <AppContainer />,
   document.getElementById('root')
 );
