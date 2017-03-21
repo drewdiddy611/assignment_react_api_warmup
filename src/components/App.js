2c4532b2ef56b6848e384c6716aef4c9edce9757
Sia <siakaramalegos@gmail.com>
2017-03-21 15:07:58 -0500

    Change app to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import JumbotronFluid from './elements/JumbotronFluid'
 import UserList from './UserList'
 import UserForm from './UserForm'
 
-class App extends Component {
-
-  render() {
-    const {users, isFetching, error, onAddUser} = this.props
-
-    return (
-      <div className="App">
-        <JumbotronFluid
-          heading="User CRUD"
-          lead="Using an API for User CRUD operations in React Applications"
-        />
-        <UserList
-          users={users}
-          isFetching={isFetching}
-        />
-        <br />
-        <UserForm
-          onSubmit={onAddUser}
-          error={error}
-        />
-      </div>
-    )
-  }
-}
+const App = ({users, isFetching, error, onAddUser}) => (
+  <div className="App">
+    <JumbotronFluid
+      heading="User CRUD"
+      lead="Using an API for User CRUD operations in React Applications"
+    />
+    <UserList
+      users={users}
+      isFetching={isFetching}
+    />
+    <br />
+    <UserForm
+      onSubmit={onAddUser}
+      error={error}
+    />
+  </div>
+)
 
 export default App
