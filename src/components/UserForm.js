a10397471606f0390dd23d2c278b20771e25ab99
Greg Filipczak <gregfilipczak@gmail.com>
2017-08-23 18:01:42 +0000

    adding bug

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,56 +1,57 @@
-import React from 'react';
-import Input from './elements/Input';
-import InputGroup from './elements/InputGroup';
-import Button from './elements/Button';
-import Alert from './elements/Alert';
-import Showable from './elements/Showable';
+import React from "react";
+import Input from "./elements/Input";
+import InputGroup from "./elements/InputGroup";
+import Button from "./elements/Button";
+import Alert from "./elements/Alert";
+import Showable from "./elements/Showable";
 
 class UserForm extends React.Component {
-	onTextChanged = e => {
-		this.props.onUserFormTextChanged(e);
-	};
+  onTextChanged = e => {
+    this.props.onUserFormTextChanged(e);
+  };
 
-	onUserFormSubmit = e => {
-		this.props.onUserFormSubmit(e);
-	};
+  onUserFormSubmit = e => {
+    this.props.onUserFormSubmit(e);
+  };
 
-	render() {
-		const { first_name, last_name, avatar } = this.props.user;
-		const { onDataSetChanged, error } = this.props;
-		return (
-			<form className="container" onSubmit={onDataSetChanged(this.props.type)}>
-				<h1>
-					{this.props.type !== UserForm.STATE_EDIT ? 'Add New' : 'Edit'} User
-				</h1>
-				<Showable show={error}>
-					<Alert type="danger">Oops, there was a problem...</Alert>
-				</Showable>
-				<InputGroup name="first_name" labelText="First Name">
-					<Input
-						name="first_name"
-						value={first_name}
-						onChange={this.onTextChanged}
-					/>
-				</InputGroup>
-				<InputGroup name="last_name" labelText="Last Name">
-					<Input
-						name="last_name"
-						value={last_name}
-						onChange={this.onTextChanged}
-					/>
-				</InputGroup>
-				<InputGroup name="avatar" labelText="Photo Link">
-					<Input name="avatar" value={avatar} onChange={this.onTextChanged} />
-				</InputGroup>
-				<Button type="submit" onClick={this.onUserFormSubmit} color="primary">
-					Save User
-				</Button>
-			</form>
-		);
-	}
+  render() {
+    const { first_name, last_name, avatar, id } = this.props.user;
+    const { onDataSetChanged, error } = this.props;
+    return (
+      <form className="container" onSubmit={onDataSetChanged(this.props.type)}>
+        <h1>
+          {this.props.type !== UserForm.STATE_EDIT ? "Add New" : "Edit"} User
+        </h1>
+        <Showable show={error}>
+          <Alert type="danger">Oops, there was a problem...</Alert>
+        </Showable>
+        <InputGroup name="first_name" labelText="First Name">
+          <Input
+            name="first_name"
+            value={first_name}
+            onChange={this.onTextChanged}
+          />
+        </InputGroup>
+        <InputGroup name="last_name" labelText="Last Name">
+          <Input
+            name="last_name"
+            value={last_name}
+            onChange={this.onTextChanged}
+          />
+        </InputGroup>
+        <InputGroup name="avatar" labelText="Photo Link">
+          <Input name="avatar" value={avatar} onChange={this.onTextChanged} />
+        </InputGroup>
+        {id ? <input type="hidden" name="id" value={id} /> : ""}
+        <Button type="submit" onClick={this.onUserFormSubmit} color="primary">
+          Save User
+        </Button>
+      </form>
+    );
+  }
 }
 
-UserForm.STATE_ADD = 'add';
-UserForm.STATE_EDIT = 'edit';
+UserForm.STATE_ADD = "add";
+UserForm.STATE_EDIT = "edit";
 
 export default UserForm;
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,161 +1,177 @@
-import React, { Component } from 'react';
-import App from '../components/App';
-import serialize from 'form-serialize';
+import React, { Component } from "react";
+import App from "../components/App";
+import serialize from "form-serialize";
 
 class AppContainer extends Component {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		// Initialize users in state as an empty array and
-		// set isFetching to false.
-		this.state = {
-			users: [],
-			isFetching: false,
-			error: null
-		};
-	}
+    // Initialize users in state as an empty array and
+    // set isFetching to false.
+    this.state = {
+      users: [],
+      isFetching: false,
+      error: null
+    };
+  }
 
-	componentDidMount() {
-		// Before performing the fetch, set isFetching to true
-		this.setState({ isFetching: true });
+  componentDidMount() {
+    // Before performing the fetch, set isFetching to true
+    this.setState({ isFetching: true });
 
-		// After component mounts, call the API to get the
-		// users, then update state which triggers re-render.
-		// Add a delay to the URL and reset isFetching upon
-		// completion of the request.
-		fetch('https://reqres.in/api/users?delay=1')
-			.then(response => response.json())
-			.then(json => {
-				this.setState({
-					users: json.data,
-					isFetching: false
-				});
-			});
-	}
+    // After component mounts, call the API to get the
+    // users, then update state which triggers re-render.
+    // Add a delay to the URL and reset isFetching upon
+    // completion of the request.
+    fetch("https://reqres.in/api/users?delay=1")
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          users: json.data,
+          isFetching: false
+        });
+      });
+  }
 
-	onDataSetChanged = type => async e => {
-		e.preventDefault();
-		const form = e.target;
+  onDataSetChanged = type => async e => {
+    e.preventDefault();
+    const form = e.target;
 
-		// Create headers to set the content type to json
-		const headers = new Headers();
-		headers.append('Content-Type', 'application/json');
+    // Create headers to set the content type to json
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
 
-		// Form body.
-		const body = serialize(form, { hash: true });
-		const options = {
-			headers: headers,
-			method: type === 'add' ? 'POST' : 'PUT',
-			body: JSON.stringify(body)
-		};
+    // Form body.
+    const body = serialize(form, { hash: true });
+    const id = body.id;
+    const options = {
+      headers: headers,
+      method: type === "add" ? "POST" : "PUT",
+      body: JSON.stringify(body)
+    };
 
-		// Before performing the fetch, set isFetching to true
-		this.setState({ isFetching: true });
+    // Before performing the fetch, set isFetching to true
+    this.setState({ isFetching: true });
 
-		try {
-			let response = await fetch('https://reqres.in/api/users', options);
-			if (!response.ok) {
-				throw new Error(`${response.status} ${response.statusText}`);
-			}
+    try {
+      let url = "https://reqres.in/api/users";
+      if (id && typeof id === "string") {
+        url += `/${id}`;
+      }
+      let response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
 
-			let json = await response.json();
-			this.setState(
-				{
-					isFetching: false,
-					users: [...this.state.users, json]
-				},
-				() => {
-					if (form && type !== 'delete') form.reset();
-				}
-			);
-		} catch (error) {
-			console.log(error);
-			this.setState({
-				isFetching: false,
-				error
-			});
-		}
-	};
+      let json = await response.json();
+      console.log(json);
+      let users;
+      if (id) {
+        //edit
+        const index = this.state.users.findIndex(user => user.id === id);
+        console.log(index);
+        users = this.state.users.splice(index, 1, json);
+      } else {
+        users = [...this.state.users, json];
+      }
 
-	// onAddUser = async e => {
-	// 	e.preventDefault();
-	// 	const form = e.target;
-	// 	const body = serialize(form, { hash: true });
-	//
-	// 	// Create headers to set the content type to json
-	// 	const headers = new Headers();
-	// 	headers.append('Content-Type', 'application/json');
-	//
-	// 	// Set options, and stringify the body to JSON
-	// 	const options = {
-	// 		headers,
-	// 		method: 'POST',
-	// 		body: JSON.stringify(body)
-	// 	};
-	//
-	// 	// Before performing the fetch, set isFetching to true
-	// 	this.setState({ isFetching: true });
-	//
-	// 	try {
-	// 		let response = await fetch('https://reqres.in/api/users', options);
-	// 		if (!response.ok) {
-	// 			throw new Error(`${response.status} ${response.statusText}`);
-	// 		}
-	// 		let json = await response.json();
-	// 		this.setState(
-	// 			{
-	// 				isFetching: false,
-	// 				users: [...this.state.users, json]
-	// 			},
-	// 			() => {
-	// 				form.reset();
-	// 			}
-	// 		);
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 		this.setState({
-	// 			isFetching: false,
-	// 			error
-	// 		});
-	// 	}
-	// };
-	//
-	onDeleteUser = async () => {
-		const options = {
-			method: 'DELETE'
-		};
+      this.setState(
+        {
+          isFetching: false,
+          users
+        },
+        () => {
+          if (form && type !== "delete") form.reset();
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        isFetching: false,
+        error
+      });
+    }
+  };
 
-		this.setState({
-			isFetching: true
-		});
-		try {
-			let response = await fetch('https://reqres.in/api/users', options);
-			if (!response.ok) {
-				throw new Error(`${response.status} ${response.statusText}`);
-			}
-			let json = await response.json();
-			this.setState({
-				isFetching: false,
-				users: [...this.state.users, json]
-			});
-		} catch (error) {
-			console.log(error);
-			this.setState({
-				isFetching: false,
-				error
-			});
-		}
-	};
+  // onAddUser = async e => {
+  // 	e.preventDefault();
+  // 	const form = e.target;
+  // 	const body = serialize(form, { hash: true });
+  //
+  // 	// Create headers to set the content type to json
+  // 	const headers = new Headers();
+  // 	headers.append('Content-Type', 'application/json');
+  //
+  // 	// Set options, and stringify the body to JSON
+  // 	const options = {
+  // 		headers,
+  // 		method: 'POST',
+  // 		body: JSON.stringify(body)
+  // 	};
+  //
+  // 	// Before performing the fetch, set isFetching to true
+  // 	this.setState({ isFetching: true });
+  //
+  // 	try {
+  // 		let response = await fetch('https://reqres.in/api/users', options);
+  // 		if (!response.ok) {
+  // 			throw new Error(`${response.status} ${response.statusText}`);
+  // 		}
+  // 		let json = await response.json();
+  // 		this.setState(
+  // 			{
+  // 				isFetching: false,
+  // 				users: [...this.state.users, json]
+  // 			},
+  // 			() => {
+  // 				form.reset();
+  // 			}
+  // 		);
+  // 	} catch (error) {
+  // 		console.log(error);
+  // 		this.setState({
+  // 			isFetching: false,
+  // 			error
+  // 		});
+  // 	}
+  // };
+  //
+  onDeleteUser = async () => {
+    const options = {
+      method: "DELETE"
+    };
 
-	render() {
-		return (
-			<App
-				onDataSetChanged={this.onDataSetChanged}
-				onDeleteUser={this.onDeleteUser}
-				{...this.state}
-			/>
-		);
-	}
+    this.setState({
+      isFetching: true
+    });
+    try {
+      let response = await fetch("https://reqres.in/api/users", options);
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      let json = await response.json();
+      this.setState({
+        isFetching: false,
+        users: [...this.state.users, json]
+      });
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        isFetching: false,
+        error
+      });
+    }
+  };
+
+  render() {
+    return (
+      <App
+        onDataSetChanged={this.onDataSetChanged}
+        onDeleteUser={this.onDeleteUser}
+        {...this.state}
+      />
+    );
+  }
 }
 
 export default AppContainer;
